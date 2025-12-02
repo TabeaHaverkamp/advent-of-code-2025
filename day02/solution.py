51524ec5ba48e097178f21475c5c45685367079f
Tabea Haverkamp <tabea@haverkamp-os.de>
2025-12-02 08:58:07 +0100

    little bit of time optimization

--- day02/solution.py
+++ day02/solution.py
@@ -1,3 +1,7 @@
+import sys, os
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_root not in sys.path: sys.path.append(project_root)
+from utils.utils import time_function 
 
 def parse_input(file_path):
     with open(file_path, "r") as file:
@@ -8,44 +12,41 @@ def parse_input(file_path):
                 ]for line in file]
     return data[0]
 
+@time_function
 def solution1():
     input = parse_input('input.txt')
     invalid_sum = 0
     for i,j in input:
         for test in range(i, j+1):
-            test = str(test)
-            if len(test)%2 == 0:
-                a =  test[0:len(test)//2]
-                b = test[len(test)//2:]
+
+            test_str = str(test)
+            if len(test_str)%2 == 0:
+                a = test_str[0:len(test_str)//2]
+                b = test_str[len(test_str)//2:]
                 if a==b:
-                    invalid_sum += int(a+b)
-    return invalid_sum
+                    invalid_sum += test
+    print(f"solution 1: {invalid_sum}")
 
 
 
-
+@time_function
 def solution2():
     def _chunkstring(string, length):
-        return list(string[0+i:length+i] for i in range(0, len(string), length))
+        return {string[0+i:length+i] for i in range(0, len(string), length)}
 
 
     input = parse_input('input.txt')
     invalid_sum = 0
     for i,j in input:
         for test in range(i, j+1):
-            test = str(test)
-            window_max_length =len(test)//2
-
-            for w in range(1, window_max_length+1):
-                chunks = _chunkstring(test, w)
-                set_chunks = set(chunks)
-                if ((len(chunks)>2 and len(set_chunks)==1) 
-                    or (len(chunks)<=2 and chunks[0]==chunks[1])
-                ):
-                    invalid_sum+=int(test)
-                    break
-    print(invalid_sum)
+            test_str = str(test)        
+            for w in range(1, len(test_str)//2+1): # batches can max be half as long as the string
+                if len(test_str)%w ==0: # the batches need to be even sized.
+                    if len(_chunkstring(test_str, w))==1 :
+                        invalid_sum+=test
+                        break
+    print(f"solution 2: {invalid_sum}")
 
 
-                
+solution1()             
 solution2()
