b7ded80e1ed8fedae9e8ecf48354f47624f06795
Tabea Haverkamp <tabea@haverkamp-os.de>
2025-12-02 08:20:59 +0100

    day2, part 2

--- day02/solution.py
+++ day02/solution.py
@@ -24,4 +24,28 @@ def solution1():
 
 
 
-solution1()
+def solution2():
+    def _chunkstring(string, length):
+        return list(string[0+i:length+i] for i in range(0, len(string), length))
+
+
+    input = parse_input('input.txt')
+    invalid_sum = 0
+    for i,j in input:
+        for test in range(i, j+1):
+            test = str(test)
+            window_max_length =len(test)//2
+
+            for w in range(1, window_max_length+1):
+                chunks = _chunkstring(test, w)
+                set_chunks = set(chunks)
+                if ((len(chunks)>2 and len(set_chunks)==1) 
+                    or (len(chunks)<=2 and chunks[0]==chunks[1])
+                ):
+                    invalid_sum+=int(test)
+                    break
+    print(invalid_sum)
+
+
+                
+solution2()
