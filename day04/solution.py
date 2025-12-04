c27b92b5cd3cc9ec46e17289ece4c31e3c1ab738
Tabea Haverkamp <tabea@haverkamp-os.de>
2025-12-04 09:40:00 +0100

    nicer code

--- day04/solution.py
+++ day04/solution.py
@@ -21,41 +21,29 @@ directions = {"L": (0, -1), "R": (0,1), "U": (-1,0), "D": (1,0),
 paper = "@"
 empty = "."
 
-
-def check_neighbours(r,c, input):
+def check_neighbours(r,c, papers):
     papers_in_neighbourhood = 0
-    _directions = directions.copy()
-    if r == 0:
-        _directions.pop("U", None)
-        _directions.pop("LU", None)
-        _directions.pop("RU", None)
-    if c == 0:
-        _directions.pop("L", None)
-        _directions.pop("LU", None)
-        _directions.pop("LD", None)
-    if r == len(input[0])-1:
-        _directions.pop("D", None)
-        _directions.pop("LD", None)
-        _directions.pop("RD", None)
-    if c == len(input)-1:
-        _directions.pop("R", None)
-        _directions.pop("RD", None)
-        _directions.pop("RU", None)  
-    for d in _directions.values():
-        if input[r+d[0]][c+d[1]] in (paper):
+    for d in directions.values():
+        if (r+d[0],c+d[1]) in papers:
             papers_in_neighbourhood +=1
     return papers_in_neighbourhood
+
 
 @time_function
 def solution1():
     access = 0
     input = parse_input("input.txt")
+    papers = set()
     for r_idx,row in enumerate(input):
         for c_idx, item in enumerate(row):
             if item == paper:
-                if check_neighbours(r_idx, c_idx, input) < 4:
-                    access += 1
+                papers.add((r_idx, c_idx))
+    
+    for r, c in papers:
+        if check_neighbours(r, c, papers=papers) < 4:
+            access += 1
     print(f"solution 1: {access}")
+
 
 
 import time
@@ -63,18 +51,23 @@ import time
 def solution2():
     access = 0
     input = parse_input("input.txt")
-    while True:
-        papers= []
-        for r_idx,row in enumerate(input):
+    paper_list = set()
+    for r_idx,row in enumerate(input):
             for c_idx, item in enumerate(row):
                 if item == paper:
-                    if check_neighbours(r_idx, c_idx, input) < 4:
-                        access += 1
-                        papers.append((r_idx, c_idx))
-        if len(papers) == 0:
-            break
-        for (r,c) in papers:
-            input[r][c]=empty
+                    paper_list.add((r_idx, c_idx))
+
+    while True:
+        remaining_paper_list = set()
+        i = 0
+        for (r_idx, c_idx) in paper_list:
+            if check_neighbours(r_idx, c_idx, paper_list) < 4:
+                access += 1
+                i+=1
+            else: remaining_paper_list.add((r_idx, c_idx))
+      
+        paper_list = remaining_paper_list
+        if i ==0: break 
             
     print(f"solution 2: {access}")
         
