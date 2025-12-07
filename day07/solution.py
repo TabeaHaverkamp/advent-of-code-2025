5dc1366e4413411f84af61132759201d2284051a
Tabea Haverkamp <tabea@haverkamp-os.de>
2025-12-07 16:28:12 +0100

    day 7 part 2

--- day07/solution.py
+++ day07/solution.py
@@ -21,15 +21,12 @@ def parse_input(file_path):
 @time_function
 def solution1():
     data, splitters, start = parse_input('input.txt')
-    q = deque()
     rays = set()
     rays.add(start[1])
     split_cnt = 0
     for row in range(1,len(data[0])):
-        first_splitter = splitters[0]
-        if first_splitter[0] == row: # if  are splitters in this row
+        if splitters[0][0] == row: # if  are splitters in this row
             while len(splitters) > 0 and splitters[0][0]== row:
-
                 spl = splitters.pop(0)[1]                
                 if spl in rays:
                     if spl == 0:
@@ -44,4 +41,41 @@ def solution1():
     print(f"solution 1: {split_cnt}")
                 
 
+
+@time_function
+def solution2():
+    data, splitters, start = parse_input('input.txt')
+    rays = set()
+    rays.add(start[1])
+    ray_counts = {start[1]:1}
+
+    for row in range(1,len(data[0])):
+        if splitters[0][0] == row: # if  are splitters in this row
+            while len(splitters) > 0 and splitters[0][0]== row:
+                spl = splitters.pop(0)[1]                
+                if spl in rays:
+                    incoming_cnt = ray_counts[spl]
+                    ray_counts[spl]=0
+                    if spl == 0:
+                        ray_counts[spl+1] = ray_counts.get(spl+1,0) + incoming_cnt
+                        rays.add(spl+1)
+                    elif spl == len(data[0]):
+                        rays.add(spl-1)
+                        ray_counts[spl-1] = ray_counts.get(spl-1,0) + incoming_cnt
+
+                    else: 
+                        ray_counts[spl+1] = ray_counts.get(spl+1,0) + incoming_cnt
+                        ray_counts[spl-1] = ray_counts.get(spl-1,0) + incoming_cnt
+                        rays.add(spl+1)
+                        rays.add(spl-1)
+                    rays.remove(spl)
+                    
+    
+    print(f"solution 2: {sum(ray_counts.values())}")
+
 solution1()
+solution2()
+
+
+
+# 3106 too low
