df4b6da50700c63246f928885b42964c54dc8b33
Tabea Haverkamp <tabea@haverkamp-os.de>
2025-12-08 15:02:38 +0100

    day8, brute force.

--- day08/solution.py
+++ day08/solution.py
@@ -69,10 +69,89 @@ def solution1():
             all_groups.append(current_group)
     group_size = [len(group) for group in all_groups]
     group_size.sort()
+    print(all_groups)
+    print(group_size)
     print(f"Solution 1: {math.prod(group_size[-3:])}")
     
 
 
 
 
-solution1()
+
+
+
+
+@time_function
+def solution2():
+
+    input_type = 'input'
+
+    coordinates = parse_input(f'{input_type}.txt')
+    distances = dict()
+    for c in range(len(coordinates)):
+        for c2 in range(len(coordinates)):
+            if c != c2:
+                #distances are not directed, so no need to compute all again
+                if not distances.get((c2,c)):
+                    distances[(c,c2)] = distance(coordinates[c],coordinates[c2])
+
+   
+
+    def recursive_dfs(connection_dict, current_key, current_group,visited_set ):
+        visited_set.add(current_key)
+        current_group.append(current_key)
+        for value in connection_dict.get(current_key, set()):
+            if value in connection_dict.keys() and value not in visited_set:
+                if value not in visited_set:
+                    recursive_dfs(connection_dict, value, current_group,visited_set)
+    
+
+    group_sizes = [0]
+    connections = dict()
+    cnt = 0
+    last_added_coord = tuple()
+    while  group_sizes[0] < len(coordinates) and cnt < 10000:
+        # get all minimum connections, so we can build nets later on
+        cnt +=1
+        min_dist = min(distances.values())
+        result = [k for k, v in distances.items() if v == min_dist] 
+
+        for i in result:
+            (x,y)  = i
+            if x in connections.keys():
+                connections[x].add(y)
+            else: connections[x] = {y}
+            
+            if y in connections.keys():
+                connections[y].add(x)
+            else:
+                connections[y] = {x}
+
+            distances.pop(i)
+            last_added_coord = (coordinates[x],coordinates[y])
+
+        all_groups = []
+        visited = set()
+        for node in connections.keys():
+            if node not in visited:
+                current_group = []
+                recursive_dfs(connections, node, current_group, visited)
+                all_groups.append(current_group)
+        group_sizes = [len(group) for group in all_groups]
+        group_sizes.sort()
+
+        # need to remove nodes that are already in network. to speed it up in the end.
+
+
+        # print("all groups", all_groups)
+        # print("group sizes", group_sizes)
+        # print("connections", connections)
+        # print("last added coord", last_added_coord)
+    print(f"Solution 2: {last_added_coord[0][0]* last_added_coord[1][0]}")
+    
+
+
+
+
+#solution1()
+solution2()
